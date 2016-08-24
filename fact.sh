168ca4d79191bf7ea521f0e120623aa74be96f02
sowmya <sowmya.almaipet@gytechs.com>
2016-08-24 10:30:46 +0530

    modified fact.sh

--- fact.sh
+++ fact.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # fact.sh - Shell script to to find factorial of given command line arg
-factorial()
+fact()
 {
   local i=$1
   local f
@@ -9,7 +9,7 @@ factorial()
   
   # factorial() is called until the value of $f is returned and is it is <= 2
   # This is called the recursion
-  [ $i -le 2 ] && echo $i || { f=$(( i - 1)); f=$(factorial $f); f=$(( f * i )); echo $f; }
+  [ $i -le 2 ] && echo $i || { f=$(( i - 1)); f=$(fact $f); f=$(( f * i )); echo $f; }
 }
 
 
@@ -17,4 +17,4 @@ factorial()
 [ $# -eq 0 ] && { echo "Usage: $0 number"; exit 1; }
 
 # call factorial
-factorial $1
+fact $1
